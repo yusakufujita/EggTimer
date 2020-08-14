98d165c2eab390354b4b99c5f2247a2eb723ad2d
masa-321 <ma.ly321.luna.notte@gmail.com>
2020-08-14 19:56:38 +0900

    選択したHardnessに応じた数値をコンソールに出力するように、DictionaryやSwitch文を使用して構築。

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+//    let softTime = 5
+//    let mediumTime = 7
+//    let hardTime = 12
+    
+    //同じコードを３つ書く必要はない…！
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        //辞書を使うことでより一行で表記できる。
+        let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+        
+//        //if文で記載
+//        if hardness == "Soft" {
+//            print(softTime)
+//        } else if hardness == "Medium" {
+//            print(mediumTime)
+//        } else {
+//            print(hardTime)
+//        }
+        
+        //Switch
+        switch hardness {
+        case "Soft":
+            print(eggTimes["Soft"])//print(softTime)
+        case "Medium":
+            print(eggTimes["Medium"])//print(mediumTime)
+        case "Hard":
+            print(eggTimes["Hard"])//print(hardTime)
+        default:
+            print("NothingSelected")
+        }
+    }
+    
 
 
 }
